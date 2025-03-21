3000c7892a180578b27d416fa514b8828a565ab1
Kylle4u <kyllepahimutang123@gmail.com>
2025-03-22 02:05:43 +0800

    Add files via upload

---
+++ register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhkHvJvvxngZkw8Exg1nz4yh5gOW12480",
+  authDomain: "logincp2.firebaseapp.com",
+  projectId: "logincp2",
+  storageBucket: "logincp2.firebasestorage.app",
+  messagingSenderId: "791811696995",
+  appId: "1:791811696995:web:e61502496a8ef6d9e5cb56"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
